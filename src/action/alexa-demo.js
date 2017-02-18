023edd00bef009ff588d5bb2e5122a43b256f143
Dragos Dascalita Haut <ddragosd@gmail.com>
2017-02-17 17:16:42 -0800

    completed a demo intent end to end

--- src/action/alexa-demo.js
+++ src/action/alexa-demo.js
@@ -40,25 +40,30 @@ var MEASUREMENT = {
 
 var AlexaSDK = require('alexa-sdk');
 var DateUtil = require('./alexa-date-util');
+var util = require('util');
 
 var handlers = {
     'LaunchRequest': function () {
         this.emit(':ask', 'Welcome to Adobe Analytics.. Which report suite would you like to use? ' + getReportSuites(), "You can choose from the following report suites" + getReportSuites());
-    },    
+    },
     'ReportSuiteSelectionIntent': function () {
 
         this.emit(':ask', 'Ok. How can I help you?', 'I can currently tell you information about X, Y and Z');
-    },    
+    },
     'OneshotReportIntent': function () {
         handleOneshotReportRequest(this.event.request.intent, this.event.session.user.accessToken);
     },
     'PageViewsToday': function() {
-        getMetric("metrics/pageviews", "today", function metricResponseCallback(err, reportResponse) {
+        var duration = "today";
+        var emit = this.emit;
+        getMetric("metrics/pageviews", duration, function metricResponseCallback(err, reportResponse) {
             var speechOutput;
 
             if (err) {
                 speechOutput = "Sorry, Adobe Analytics experienced an error. Please try again later";
+                console.log("error:" + err.toString());
             } else {
+                console.log("report response:" + reportResponse);
                 var verb;
                 if(duration == "today" || duration == "this week" || duration == "this month" || duration == "this year"){
                     verb = "is";
@@ -66,15 +71,17 @@ var handlers = {
                     verb = "was";
                 }
 
-                var measurement = getMeasurementFromIntent(intent);
+                /*var measurement = getMeasurementFromIntent(intent);
                 if(metric.query.indexOf("average") > -1){
                     speechOutput = "The " + metric.query + " " + duration + " " + verb + " " + parseFloat(reportResponse).toFixed(2) + " " + measurement + ".";
                 }else{
                     speechOutput = "The total number of " + metric.query + " " + duration + " " + verb + " " + reportResponse;
-                }
+                }*/
+
+                speechOutput = "The total number is " + reportResponse;
             }
 
-            this.emit(':tell', speechOutput, false);
+            emit(':tell', speechOutput, false);
         });
     },
     'ThankYouIntent': function () {
@@ -137,26 +144,34 @@ function getMetric(metric, duration, metricResponseCallback) {
     console.log("From Date " + durationDates.fromDate);
     console.log("To Date " + durationDates.toDate);
 
-    var headers = { "Authorization" : "Bearer eyJ4NXUiOiJpbXNfbmExLWtleS0xLmNlciIsImFsZyI6IlJTMjU2In0.eyJmZyI6IlJGN1Q2TDROUE1BQUFBQUFBQUFBQUFHWUFBPT09PT09IiwiYyI6Ilg5NGhjWmpwUzlwZEp4dzZWS3MyTmc9PSIsIm1vaSI6IjQ4NmUwYzkyIiwicnRpZCI6IjE0ODczNDUyMzU0MDgtNGVjNmVlNDctMzc3MS00NDNhLTg1ZmUtZGFhMGQ3MmI2MGYxIiwiY3JlYXRlZF9hdCI6IjE0ODczNDUyMzU0MDgiLCJydGVhIjoiMTQ4ODU1NDgzNTQwOCIsInR5cGUiOiJhY2Nlc3NfdG9rZW4iLCJjbGllbnRfaWQiOiJTaXRlQ2F0YWx5c3QyIiwiYXMiOiJpbXMtbmExIiwib2MiOiJyZW5nYSpuYTFyKjE1YTRjYWYzMWVkKjBCNkFTNTlGSlgyMFhFMTJRWlFIUzc2NlNHIiwidXNlcl9pZCI6IjEwNzMzNDk1NEE3MDY4RTc5OTIwMTVBOUBBZG9iZUlEIiwic2NvcGUiOiJvcGVuaWQsQWRvYmVJRCxyZWFkX29yZ2FuaXphdGlvbnMsYWRkaXRpb25hbF9pbmZvLnByb2plY3RlZFByb2R1Y3RDb250ZXh0LGFkZGl0aW9uYWxfaW5mby5qb2JfZnVuY3Rpb24sc2Vzc2lvbiIsImlkIjoiMTQ4NzM0NTIzNTQwOC00MjJiOGMxMy02YTE2LTRlYjMtYjI2MS1jNTgxMDI4MGI3NzEiLCJzdGF0ZSI6IntcInNlc3Npb25cIjpcImh0dHBzOi8vaW1zLW5hMS5hZG9iZWxvZ2luLmNvbS9pbXMvc2Vzc2lvbi92MS9PVE00TlRjeU1qWXRPRE01T0MwMFlqYzNMVGswT1RFdE5tTTBPR0l3TkdZeU5Ua3lMUzB4TURjek16UTVOVFJCTnpBMk9FVTNPVGt5TURFMVFUbEFRV1J2WW1WSlJBXCJ9IiwiZXhwaXJlc19pbiI6Ijg2NDAwMDAwIn0.DoMFI9dm6WbHliyEtLkdDTQAmAeW0fLmMgMTrmUo1hp1EFsXEbSxvYf_GY3nuTCgEYrRUfjNkcNqaXjSHsvgZjPY7KduFkQMtsmxBgsMnnq08sigZtVMB1ed1sYuiWp3xyy39g_XXXvNa3rvzX9sPxmk1VlmW_4kBW32A2iYVR0EItfgSLOo5Ayo3uCmP0PbsQ3Y6-mTVGyRfs7YMt93fU0Vcr9qpoGSBkuEBfUsLXcxQQwHvgx31QTITnVvvBGGdzGhvf4kKcoLUYGuNQ0RdPfS2hzgzyc82vJT_Mw1lXRorP_vo6uD6_tZjcEv1VqdT6Se9vCMQeo8bgYweFR-5A",
-                    "x-api-key" : "analytics-services",
-                    "x-proxy-company" : "AdobeAtAdobe" }
+    var headers = {
+        "Authorization": "Bearer eyJ4NXUiOiJpbXNfbmExLWtleS0xLmNlciIsImFsZyI6IlJTMjU2In0.eyJmZyI6IlJGN1Q2TDROUE1BQUFBQUFBQUFBQUFHWUFBPT09PT09IiwiYyI6Ilg5NGhjWmpwUzlwZEp4dzZWS3MyTmc9PSIsIm1vaSI6IjQ4NmUwYzkyIiwicnRpZCI6IjE0ODczNDUyMzU0MDgtNGVjNmVlNDctMzc3MS00NDNhLTg1ZmUtZGFhMGQ3MmI2MGYxIiwiY3JlYXRlZF9hdCI6IjE0ODczNDUyMzU0MDgiLCJydGVhIjoiMTQ4ODU1NDgzNTQwOCIsInR5cGUiOiJhY2Nlc3NfdG9rZW4iLCJjbGllbnRfaWQiOiJTaXRlQ2F0YWx5c3QyIiwiYXMiOiJpbXMtbmExIiwib2MiOiJyZW5nYSpuYTFyKjE1YTRjYWYzMWVkKjBCNkFTNTlGSlgyMFhFMTJRWlFIUzc2NlNHIiwidXNlcl9pZCI6IjEwNzMzNDk1NEE3MDY4RTc5OTIwMTVBOUBBZG9iZUlEIiwic2NvcGUiOiJvcGVuaWQsQWRvYmVJRCxyZWFkX29yZ2FuaXphdGlvbnMsYWRkaXRpb25hbF9pbmZvLnByb2plY3RlZFByb2R1Y3RDb250ZXh0LGFkZGl0aW9uYWxfaW5mby5qb2JfZnVuY3Rpb24sc2Vzc2lvbiIsImlkIjoiMTQ4NzM0NTIzNTQwOC00MjJiOGMxMy02YTE2LTRlYjMtYjI2MS1jNTgxMDI4MGI3NzEiLCJzdGF0ZSI6IntcInNlc3Npb25cIjpcImh0dHBzOi8vaW1zLW5hMS5hZG9iZWxvZ2luLmNvbS9pbXMvc2Vzc2lvbi92MS9PVE00TlRjeU1qWXRPRE01T0MwMFlqYzNMVGswT1RFdE5tTTBPR0l3TkdZeU5Ua3lMUzB4TURjek16UTVOVFJCTnpBMk9FVTNPVGt5TURFMVFUbEFRV1J2WW1WSlJBXCJ9IiwiZXhwaXJlc19pbiI6Ijg2NDAwMDAwIn0.DoMFI9dm6WbHliyEtLkdDTQAmAeW0fLmMgMTrmUo1hp1EFsXEbSxvYf_GY3nuTCgEYrRUfjNkcNqaXjSHsvgZjPY7KduFkQMtsmxBgsMnnq08sigZtVMB1ed1sYuiWp3xyy39g_XXXvNa3rvzX9sPxmk1VlmW_4kBW32A2iYVR0EItfgSLOo5Ayo3uCmP0PbsQ3Y6-mTVGyRfs7YMt93fU0Vcr9qpoGSBkuEBfUsLXcxQQwHvgx31QTITnVvvBGGdzGhvf4kKcoLUYGuNQ0RdPfS2hzgzyc82vJT_Mw1lXRorP_vo6uD6_tZjcEv1VqdT6Se9vCMQeo8bgYweFR-5A",
+        "x-api-key": "analytics-services",
+        "x-proxy-company": "AdobeAtAdobe"
+    }
 
     var analytics = require('./aa-lib/adobe-analytics')(headers);
 
-    console.log("Running");
-    analytics.then(function(api){
-            var args = {"rsid":"adbecush",
-                        "globalFilters": [{ 
-                           "type": "dateRange", 
-                           "dateRange":"2017-1-1T00:00:00/2017-2-1T23:59:59"}],    
-                        "metricContainer": { "metrics": [ { "id": "metrics/visitors"} ] }}
+    analytics.then(function (api) {
+        var args = {
+            "rsid": "adbecush",
+            "globalFilters": [{
+                "type": "dateRange",
+                "dateRange": "2017-1-1T00:00:00/2017-2-1T23:59:59"
+            }],
+            "metricContainer": {"metrics": [{"id": "metrics/visitors"}]}
+        }
+        console.log("Running analytics API with args:" + util.inspect(args, {depth:4}));
 
-            api.reports.runRankedReport({'body': args}).then(function(result)
-            {
+        api.reports.runRankedReport({'body': args})
+            .then(function (result) {
                 var data = JSON.parse(result["data"]);
                 var total = data.summaryData.totals[0];
                 metricResponseCallback(null, total);
             })
+            .catch( function(error) {
+                metricResponseCallback(error,-1);
+            });
     })
 }
 
